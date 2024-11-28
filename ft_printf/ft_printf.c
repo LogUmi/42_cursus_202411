de2bc9f7062d82f9dfc6eb6a34da25ed4d276b22
Loic Gerard <lgerard@c2r1p6.42perpignan.fr>
2024-11-28 16:58:09 +0100

    sauvegarde avant reset des postes

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -3,16 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lgerard <lgerard@student.42.fr>            +#+  +:+       +#+        */
+/*   By: lgerard <lgerard@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 11:14:21 by lgerard           #+#    #+#             */
-/*   Updated: 2024/11/28 13:22:40 by lgerard          ###   ########.fr       */
+/*   Updated: 2024/11/28 16:47:25 by lgerard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
+#include "ft_print.h"
+
+static void ft_printspecs(const char *str,int *len, va_arg(args))
+
+
+static void	ft_printbacks(const char *str,int *len)
+{
+	
+}
 
 int	ft_print(const char *str, ...)
 {
+	va_list		args;
+	va_start	(args, str);
+	int			len;
 
+	len = 0;
+	while (str[len] != 0)
+	{
+		if ((unsigned char)str[len] == '%')
+			ft_printspecs(str[len++], &len, va_list);
+		else
+			if((unsigned char)str[len] == 92)
+				ft_printbacks(str[++len], &len);
+			else
+				write(1, &str[len++], 1);
+	}
+	va_end(args);
+	return (len);
+	
 }
