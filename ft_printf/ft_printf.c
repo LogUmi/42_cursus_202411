03ac988dc47664d5851862f39d9bc6af95c47fd4
Loic Gerard <lgerard@c2r1p5.42perpignan.fr>
2024-11-29 19:01:19 +0100

    version au 29/11/2024 au soir

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -3,53 +3,99 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lgerard <lgerard@student.42.fr>            +#+  +:+       +#+        */
+/*   By: lgerard <lgerard@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 11:14:21 by lgerard           #+#    #+#             */
-/*   Updated: 2024/11/29 12:11:55 by lgerard          ###   ########.fr       */
+/*   Updated: 2024/11/29 18:52:27 by lgerard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <unistd.h>
-#include "ft_printf.h"
+#include "libftprintf.h"
 
-static int	ft_printspecs(const char *str, int *len, int *nchar, va_list args)
-{
-	return (0);
-}
-
-static char	*ft_printbacksnum(const char *str, int *len, int *nchar, int *i)
-{
-	return ((char *)str);
-}
-
-static int	ft_printbacks(const char *str, int *len, int *nchar, int i)
-{
-	char	*escl;
-	char	*escc;	
-
-	escl = "ntrbf\\'\"v";
-	escc = "/10/9/13/8/12/92/39/34/11";
-	if ((str[0] > 47 && str[0] < 58) || str[0] == 'x')
+static int	ft_printspecs2(const char *str, int *len, int *nchar, va_list args)
+{	
+	char	*s;
+	
+	if (str[0] == 'x' || str[0] == 'X')
 	{
-		write(1, ft_printbacksnum(str, len, nchar, &i), i);
+		s = ft_itoa(va_arg(args, int));
+		if (!s)
+			return (-1);
+		ft_putstr_fd(s, 1);
+		(*nchar) += ft_strlen(s);
+		free(s);
+		(*len)++;
 		return (0);
 	}
-	while (escl[i] != 0)
+	if (str[0] == 's')
 	{
-		if (str[0] == escl[i])
-		{
-			write(1, &escc[i], 1);
-			(*len)++;
-			(*nchar)++;
-			return (0);
-		}
-		i++;
+		s = va_arg(args, char *);
+		ft_putstr_fd(s, 1);
+		(*nchar) += ft_strlen(s);
+		(*len)++;
+		return (0);
+	}	
+	return (ft_printspecs2( str, len, nchar, args));
+}
+
+static int	ft_printspecs1(const char *str, int *len, int *nchar, va_list args)
+{	
+	char	*s;
+	
+	if (str[0] == 'd' || str[0] == 'i')
+	{
+		s = ft_itoa(va_arg(args, int));
+		if (!s)
+			return (-1);
+		ft_putstr_fd(s, 1);
+		(*nchar) += ft_strlen(s);
+		free(s);
+		(*len)++;
+		return (0);
 	}
-	write(1, "\\", 1);
-	(*nchar)++;
-	return (0);
+	if (str[0] == 's')
+	{
+		s = va_arg(args, char *);
+		ft_putstr_fd(s, 1);
+		(*nchar) += ft_strlen(s);
+		(*len)++;
+		return (0);
+	}	
+	return (ft_printspecs2( str, len, nchar, args));
+}
+
+static int	ft_printspecs0(const char *str, int *len, int *nchar, va_list args)
+{
+	char	*s;
+
+	if (str[0] == 'c')
+	{
+		ft_putchar_fd(va_arg(args, int), 1);
+		(*nchar)++;
+		(*len)++;
+		return (0);
+	}
+	if (str[0] == 'u')
+	{
+		s = ft_itoau(va_arg(args, unsigned int));
+		if (!s)
+			return (-1);
+		ft_putstr_fd(s, 1);
+		(*nchar) += ft_strlen(s);
+		free(s);
+		(*len)++;
+		return (0);
+	}	
+	if (str[0] == '%')
+	{
+		ft_putchar_fd('%', 1);
+		(*nchar)++;
+		(*len)++;
+		return (0);
+	}
+	return (ft_printspecs1( str, len, nchar, args));
 }
 
 int	ft_printf(const char *str, ...)
@@ -63,23 +109,16 @@ int	ft_printf(const char *str, ...)
 	nchar = 0;
 	while (str[len] != 0)
 	{
-		if ((unsigned char)str[len] == '%')
+		
+		if ((unsigned char)str[len] != '%')
 		{
-			if (ft_printspecs(&str[++len], &len, &nchar, args) < 0)
-				return (-1);
+			write(1, &str[len++], 1);
+			nchar++;
 		}
 		else
-		{
-			if ((unsigned char)str[len] == 92)
-			{
-				if (ft_printbacks(&str[++len], &len, &nchar, 0) < 0)
-					return (-1);
-			}
-			else
-			{
-				write(1, &str[len++], 1);
-				nchar++;
-			}
+		{	
+			if(ft_printspecs0(&str[++len], &len, &nchar, args) < 0)
+				return (-1);
 		}	
 	}
 	va_end(args);
@@ -94,10 +133,12 @@ int	main(int argc, char **argv)
 	int i = 0;
 	int j = 1;
 
-	i = printf("01234\tend");
-	printf("... %d\x10", i);
-	i = ft_printf("01234\tend");
-	printf("... %d\x10", i);
+	i = printf("01234\t , %%, %c, %s, %s:, %d, %i, %u end", 'O',"56789%s", "", 
+		-2147483647, -2147483647, 2147483648);
+	printf("... %d\n", i);
+	i = ft_printf("01234\t , %%, %c, %s, %s:, %d, %i, %u end", 'O',"56789%s", "", 
+		-2147483647, -2147483647, 2147483648);
+	printf("... %d\n", i);
 	printf("\n\n");
 	if (argc < 2)
 		return (0);
@@ -108,6 +149,9 @@ int	main(int argc, char **argv)
 		i = ft_printf("%s", argv[j++]);
 		printf("... %d", i);
 		printf("\n\n");
-	}	
-	return (0);
+	}
+	// (void)argc;
+	// (void)argv;
+	// printf("%d\n", -2147483648);	
+	// return (0);
 }
--- ft_printf/libftprintf.h
+++ ft_printf/libftprintf.h
@@ -1,21 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lgerard <lgerard@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 12:15:47 by lgerard           #+#    #+#             */
-/*   Updated: 2024/11/28 18:22:52 by lgerard          ###   ########.fr       */
+/*   Updated: 2024/11/29 18:32:11 by lgerard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
 
 # include <stdarg.h>
 # include "libft/libft.h"
 
-int	ft_print(const char *str, ...);
+int		ft_printf(const char *str, ...);
+char	*ft_itoau(unsigned int n);
 
 #endif
