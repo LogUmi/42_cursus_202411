d82680eabb3bb87e370f79899dca9eb1596266d3
LogUmi <polarismoins@gmail.com>
2024-12-29 18:09:19 +0100

    Version apres integration de ft_printf amelioree

--- ma_libft/includes/libft.h
+++ ma_libft/includes/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lgerard <lgerard@student.42perpignan.fr    +#+  +:+       +#+        */
+/*   By: lgerard <lgerard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 15:17:27 by lgerard           #+#    #+#             */
-/*   Updated: 2024/12/20 19:14:13 by lgerard          ###   ########.fr       */
+/*   Updated: 2024/12/29 17:22:24 by lgerard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 char	*ft_itoa(int n);
+int		ft_len(long long i, int nb, int u);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 void	ft_lstclear(t_list **lst, void (*del)(void *));
@@ -45,18 +46,19 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 int		ft_printf(const char *str, ...);
-int		ft_strlenf(const char *str);
-int		ft_putstrf(const char *str);
-int		ft_printchar(int i);
-int		ft_printstr(const char *str);
-int		ft_printint(int i);
-int		ft_printunsbase(unsigned int i, char *bs);
-int		ft_putnbrullbase(unsigned long long i, char *bs);
-int 	ft_printpointer(void *p, char *bs);
+int		ft_printstr(const char *str, int *nsp);
+int		ft_putint(int i, int *ndig);
+int		ft_printunsbase(unsigned int i, char *bs, int *ndig);
+int		ft_printpointer(void *p, char *bs, int *ndig);
+int		ft_putcf(int i, int n);
+int		ft_putccf(int i, int n, int *nsp);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
+int		ft_putnsbase(unsigned int i, char *bs, int *ndig);
+int		ft_putnbrullbase(unsigned long long i, char *bs);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr_fd(char *s, int fd);
+int		ft_putstrf(const char *str, int *nsp);
 char	**ft_split(char const *s, char c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s);
@@ -65,6 +67,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlen(const char *str);
+int		ft_strlenf(const char *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_strrchr(const char *s, int c);
