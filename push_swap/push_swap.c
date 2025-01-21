0eb582ccc43cd71026ceb7ec13a2296129399b94
LogUmi <polarismoins@gmail.com>
2025-01-21 16:29:27 +0100

    version home 21/01/2025

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -3,42 +3,65 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lgerard <lgerard@student.42perpignan.fr    +#+  +:+       +#+        */
+/*   By: lgerard <lgerard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 16:22:20 by lgerard           #+#    #+#             */
-/*   Updated: 2025/01/14 19:01:03 by lgerard          ###   ########.fr       */
+/*   Updated: 2025/01/18 21:04:52 by lgerard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include push_swap.h
 
-int	ft_error(void)
+int	ft_error(char **tab)
 {
-	
+	int	i;
+
+	i = 0;
+	if (**tab)
+	{
+		while (tab[i])
+			free(tab[i++]);
+		free(**tab);
+	}
 	ft_printf("Error\n");
 	return (0);
+}
+static char	**ft_tabdup(char **tab, int dep, int nb)
+{
+	char	**new;
+	int		i;
+
+	i = 0;
+	new = malloc(sizeof(char *) * nb + 1)
+	if (!new)
+		return (ft_error(NULL));
+	while(i < nb)
+	{
+		new[i] = ft_strdup(tab[dep + i])
+		if (!new[i])
+			return (ft_error(new));
+		i++
+	}	
+	return (new);
 }
 
 int	main(int argc, char **argv)
 {
-	char	**s
+	char	**s;
 	
 	s = NULL;
 	if (argc < 3)
 	{
 		if (argc == 1)
-			return (ft_error);
+			return (0);
 		if(ft_strlen(argv[1]) < 3)
-			return (ft_error);
+			return (ft_error(s));
 		s = ft_split(argv[1], 32)
-		if (!s)
-			return(ft_error)
-	
 	}
+	
 	else
-	{
-		while (argc != 0)
-			if 
-	}
-	
+		s = ft_tabdup(argv, 1, argc - 1)
+	if (!s)
+			return (ft_error(s));
+	return (0);
 }
