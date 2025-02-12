8634d3c15893e988c55fce131959e81b7c47c970
Loic Gerard <lgerard@c1r3p1.42perpignan.fr>
2025-02-12 20:00:28 +0100

    version 42 du 12/02/2025 (minitalk)

---
+++ mini_talk/minitalk.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgerard <lgerard@student.42perpignan.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 15:05:54 by lgerard           #+#    #+#             */
+/*   Updated: 2025/02/12 19:53:50 by lgerard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "libft/includes/libft.h"
+# include <signal.h>
+# include <stdio.h>
+# include <limits.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef WAIT_LIMIT
+#  define WAIT_LIMIT 10000000
+# endif
+
+#endif
