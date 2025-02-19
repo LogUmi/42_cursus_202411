9cdbbe74d97974233853dc53c2245b53dea3d3a3
LogUmi <polarismoins@gmail.com>
2025-02-19 18:11:27 +0100

    version 42 du 19/02/2025 apm

--- fdf/macpro/fdf.h
+++ fdf/macpro/fdf.h
@@ -6,7 +6,7 @@
 /*   By: lgerard <lgerard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 16:22:35 by lgerard           #+#    #+#             */
-/*   Updated: 2025/02/18 21:20:15 by lgerard          ###   ########.fr       */
+/*   Updated: 2025/02/19 16:50:36 by lgerard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@
 # include <stdio.h>
 # include <unistd.h>
 # include "libft/includes/libft.h"
-# include "minilibx/mlx.h"
+# include "mlx/mlx.h"
 
 #endif
