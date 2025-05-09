158ff161030d05fb3cd7f3f72595c47fa88ce9df
LogUmi <polarismoins@gmail.com>
2025-05-09 11:17:16 +0200

    version home 09/05

---
+++ philo1/philosophers.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgerard <lgerard@student.42perpignan.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/01 11:34:54 by lgerard           #+#    #+#             */
+/*   Updated: 2025/05/08 13:41:13 by lgerard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+static void	*justone(t_tab *t)
+{
+	get_pmsg(t, "has taken a fork\n", 0, 0);
+	while (is_end(NULL, t) == 0)
+		usleep(1000);
+	return (NULL);
+}
+
+static void	go_sleep(t_tab *t)
+{
+	get_pmsg(t, "is sleeping\n", 0, 0);
+	usleep(t->par[3] * 1000);
+}
+
+static void	go_eat(t_tab *t)
+{
+	long long	k;
+
+	k = get_pmsg(t, "is eating\n", 0, 0);
+	pthread_mutex_lock(t->mut_lastmeal);
+	(*t->lastmeal) = k;// + t->par[2];
+	pthread_mutex_unlock(t->mut_lastmeal);
+	usleep(t->par[2] * 1000);
+	pthread_mutex_unlock(t->mut_rf);
+	pthread_mutex_unlock(t->mut_lf);
+	(*t->rfork) = 0;
+	(*t->lfork) = 0;
+	pthread_mutex_unlock(t->mut_rf);
+	pthread_mutex_unlock(t->mut_lf);
+	pthread_mutex_lock(t->mut_nmeal);
+	if ((*t->nmeal) > 0)
+		(*t->nmeal)--;
+	pthread_mutex_unlock(t->mut_nmeal);
+}
+
+static int	def_state(t_tab *t)
+{
+	if ((t->id % 2) == 0)
+	{
+		//usleep(1000);
+		return (1);
+	}
+	return (0);
+}
+
+void	*phil(void *arg)
+{
+	t_tab		*t;
+	int			state;
+
+	t = (t_tab *)arg;
+	while (is_start(NULL, t) == -1 && is_end(NULL, t) == 0)
+		usleep(200);
+	if (t->par[0] == 1)
+		return (justone(t));
+	state = def_state(t);
+	while (is_end(NULL, t) == 0)
+	{
+		if (state == 1 && is_end(NULL, t) == 0)
+			state = go_think(t, state);
+		else if (state == 0 && is_end(NULL, t) == 0
+				&& take_forks(t, state) != 0)
+			state = go_think(t, 1);
+		if (is_end(NULL, t) == 0)
+			go_eat(t);
+		if (is_end(NULL, t) == 0)
+			go_sleep(t);
+		state = 1;
+	}
+	return (NULL);
+}
---
+++ philo1/var_mutex.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   var_mutex.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgerard <lgerard@student.42perpignan.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/04 10:58:44 by lgerard           #+#    #+#             */
+/*   Updated: 2025/05/08 11:46:29 by lgerard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+long long	is_start(t_sup *s, t_tab *t)
+{
+	long long	i;
+
+	i = -1;
+	if (t)
+	{
+		pthread_mutex_lock(t->mut_start);
+		i = (*t->start_ms);
+		pthread_mutex_unlock(t->mut_start);
+	}
+	else if (s)
+	{
+		pthread_mutex_lock(&s->mut_start);
+		i = s->start_ms;
+		pthread_mutex_unlock(&s->mut_start);
+	}
+	return (i);
+}
+
+int	is_end(t_sup *s, t_tab *t)
+{
+	int	i;
+
+	i = -1;
+	if (t)
+	{
+		pthread_mutex_lock(t->mut_end);
+		i = (*t->end);
+		pthread_mutex_unlock(t->mut_end);
+	}
+	else if (s)
+	{
+		pthread_mutex_lock(&s->mut_end);
+		i = s->end;
+		pthread_mutex_unlock(&s->mut_end);
+	}
+	return (i);
+}
+
+int	is_nmeal(t_sup *s, int id)
+{
+	int	i;
+
+	i = -1;
+	pthread_mutex_lock(&s->mut_nmeal[id]);
+	i = s->nmeal[id];
+	pthread_mutex_unlock(&s->mut_nmeal[id]);
+	return (i);
+}
+
+long long	is_lastmeal(t_sup *s, int id)
+{
+	long long	i;
+
+	i = -1;
+	pthread_mutex_lock(&s->mut_lastmeal[id]);
+	i = s->lastmeal[id];
+	pthread_mutex_unlock(&s->mut_lastmeal[id]);
+	return (i);
+}
+
