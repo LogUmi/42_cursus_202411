1bc9c15df2de886716595956f05e4592e5e714b9
Loic Gerard <lgerard@c3r4p6.42perpignan.fr>
2025-07-24 18:21:00 +0200

    version 42 24/07

---
+++ CPP/00/ex00/src/megaphone.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cctype>
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{	
+		int i = 1;
+		int j = 0;
+		while (i < (argc))
+		{
+			if (i > 2)
+				std::cout << " ";
+			std::string chr = argv[i++];
+			if (chr.size() == 0)
+				std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+			else
+			{
+				std::cout << chr;
+				/* j = 0;
+					while (!chr.empty())
+					std::cout << ::toupper(chr[j++]); */
+			}
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
