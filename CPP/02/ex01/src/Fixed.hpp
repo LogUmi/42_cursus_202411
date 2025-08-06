10f55f1ae8a00228fda053376f98c3e0ca0327b5
Loic Gerard <lgerard@c1r3p4.42perpignan.fr>
2025-08-06 18:31:57 +0200

    version 42 06/08

---
+++ CPP/02/ex01/src/Fixed.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgerard <lgerard@student.42perpignan.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/04 16:09:22 by lgerard           #+#    #+#             */
+/*   Updated: 2025/08/06 14:00:20 by lgerard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class	Fixed
+{
+	public:
+		Fixed( void );
+		Fixed( const Fixed & f);
+		~Fixed( void );
+
+		Fixed&	operator=(const Fixed& f);
+		int		getRawBits( void ) const;
+		void	setRawBits( int const raw );
+
+	private:
+		int					_value;
+		static const int	_nbits = 8;
+};
+#endif
